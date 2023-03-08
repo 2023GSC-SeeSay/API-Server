16373a1ab806a3b6b4f8ec4782ce1a82f44cb4f0
ulysseyson <ulysseyson57@gmail.com>
2023-03-08 10:22:30 +0900

    basic routing & stt api routing

--- main.go
+++ main.go
@@ -1,156 +1,24 @@
-// RESTful API
-// creating, reading, updating, and deleting user profile
-
-// $ go build
-// $ pardon_v1.exe
-// => http://localhost:8080/users 에서 확인 가능
-
-// or do 
-// $ go run main.go
-
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"log"
-	"net/http"
-	"os"
-
-	"github.com/gorilla/mux"
-	"github.com/sirupsen/logrus"
+	fiber "github.com/gofiber/fiber/v2"
 )
 
-type User struct {
-	ID        string `json:"id"`
-	FirstName string `json:"firstName"`
-	LastName  string `json:"lastName"`
-	Email     string `json:"email"`
-}
 
-var users = make(map[string]User)
-var logger *logrus.Logger
-
-func init() {
-	// Create a new logger instance
-	logger = logrus.New()
-
-	// Set the logger output to stdout
-	logger.SetOutput(os.Stdout)
-
-	// Set the logger level to info
-	logger.SetLevel(logrus.InfoLevel)
-
-	// Set the logger format to JSON
-	logger.SetFormatter(&logrus.JSONFormatter{})
-}
-
-func CreateUser(w http.ResponseWriter, r *http.Request) {
-	logger.Info("Creating user...")
-
-	// Decode the request body into a User struct
-	var user User
-	err := json.NewDecoder(r.Body).Decode(&user)
-	if err != nil {
-		logger.WithError(err).Error("Failed to decode request body")
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	// Check if the user already exists
-	if _, ok := users[user.ID]; ok {
-		logger.Warn("User already exists")
-		http.Error(w, "User already exists", http.StatusBadRequest)
-		return
-	}
-
-	// Add the user to the map
-	users[user.ID] = user
-
-	// Send a success response
-	w.WriteHeader(http.StatusCreated)
-	json.NewEncoder(w).Encode(user)
-
-	logger.WithFields(logrus.Fields{
-		"user_id": user.ID,
-	}).Info("User created successfully")
-}
-
-func GetUser(w http.ResponseWriter, r *http.Request) {
-	logger.Info("Retrieving user...")
-
-	// Get the user ID from the request URL
-	vars := mux.Vars(r)
-	userID := vars["id"]
-
-	// Check if the user exists
-	user, ok := users[userID]
-	if !ok {
-		logger.WithFields(logrus.Fields{
-			"user_id": userID,
-		}).Warn("User not found")
-		http.Error(w, "User not found", http.StatusNotFound)
-		return
-	}
-
-	// Send the user profile data in the response body
-	json.NewEncoder(w).Encode(user)
-
-	logger.WithFields(logrus.Fields{
-		"user_id": user.ID,
-	}).Info("User retrieved successfully")
-}
 
 func main() {
-	// Create a new Gorilla mux router
-	r := mux.NewRouter()
 
-	// Register the CreateUser and GetUser handlers
-	r.HandleFunc("/users", CreateUser).Methods("POST")
-	r.HandleFunc("/users/{id}", GetUser).Methods("GET")
+	app := fiber.New()
 
-	// Set up the HTTP server
-	server := &http.Server{
-		Addr:    ":8080",
-		Handler: r,
-	}
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
 
-	logger.Info("Starting server on port 8080...")
-	err := server.ListenAndServe()
-	if err != nil {
-		log.Fatal(err)
-	}
+	app.Get("/:uid/audio", func(c *fiber.Ctx) error {
+		uid := c.Params("uid")
+		return c.SendString("Hello, " + uid + "!" +stt("C:\\workspace\\API-Server\\음성 002.wav", "C:\\workspace\\API-Server\\auth\\seesay-firebase-adminsdk-clpnw-faf918ab9f.json"))
+	})
+
+	app.Listen(":3000")
+
 }
-
-
-/*
- TODO
- 1. Add error handling - The current code does not handle errors that may occur 
- during request processing, such as invalid input data or database errors. 
- You can add error handling code using if statements or panic statements 
- to gracefully handle errors and return appropriate  HTTP response codes and error messages.
-
- 2. Use a database - The current code stores user profiles in a map variable in memory, 
- which is not suitable for production use. You can use a database such as MySQL, PostgreSQL, 
- or MongoDB to store user data persistently and improve performance.
-
- 3. Add authentication - The current code does not include any authentication mechanisms 
- to verify user identities or restrict access to protected endpoints. You can use a library 
- such as jwt-go or oauth2 to add authentication and authorization functionality.
-
- 4. Use a web framework - The current code uses the standard http package, which can be difficult 
- to use and requires a lot of boilerplate code. You can use a web framework such as Gin, Echo, or Beego 
- to simplify request handling, routing, middleware, and error handling.
-
- 5. Add logging - The current code does not include any logging mechanisms to record server activity or errors. 
- You can use a logging library such as logrus or zap to log server events and errors to a file or a database.
-
- 6. Use environment variables - The current code uses hard-coded values for the server address and port, 
- which can be difficult to manage in different environments (e.g. development, staging, production). 
- You can use environment variables to store configuration values and make the code more portable and flexible.
-
- 7. Write unit tests - The current code does not include any unit tests to verify 
- the correctness of the implementation and prevent regressions. You can use a testing library 
- such as testing or goconvey to write unit tests for each endpoint and HTTP handler.
-
- */
--- stt.go
+++ stt.go
@@ -1,22 +1,24 @@
-
 // Sample speech-quickstart uses the Google Cloud Speech API to transcribe
 // audio.
 package main
 
 import (
-        "context"
-        "fmt"
-        // "log"
-		"io/ioutil"
+	"context"
+	"fmt"
 
-    	"cloud.google.com/go/speech/apiv1"	
-        speechpb "cloud.google.com/go/speech/apiv1/speechpb"
-		
-		"google.golang.org/api/option"
+	// "log"
+	"io/ioutil"
+
+	speech "cloud.google.com/go/speech/apiv1"
+	speechpb "cloud.google.com/go/speech/apiv1/speechpb"
+
+	"google.golang.org/api/option"
 )
 
-func main() {
-		credFilePath := "C:\\workspace\\API-Server\\auth\\seesay-firebase-adminsdk-clpnw-faf918ab9f.json"
+func stt(audio_file_path string, cred_file_path string) string {
+		/* Transcribe the given audio file using google cloud speech api.*/
+		/* Requires audio file path & credential file path */
+		credFilePath := cred_file_path
         // Creates a client.
 		// Create a new context and load credentials from file
 		ctx := context.Background()
@@ -27,37 +29,40 @@ func main() {
 
 		if err != nil {	
 			fmt.Printf("Error creating SpeechClient: %v\n", err)
-			return
+			return ""
 		}
 		defer speechClient.Close()
 
         // The path to the remote audio file to transcribe.
         // fileURI := "gs://cloud-samples-data/speech/brooklyn_bridge.raw"
-		filePath :="C:\\Users\\aa\\OneDrive - 고려대학교\\문서\\소리 녹음\\녹음 (4).m4a"
-		audioData, err := ioutil.ReadFile(filePath)
+		// filePath :="C:\\workspace\\API-Server\\음성 002.wav"
+		audioData, err := ioutil.ReadFile(audio_file_path)
 		if err != nil {
 			fmt.Printf("Error reading audio file: %v\n", err)
-			return
+			return ""
 		}
+		// fmt.Printf("Audio data: %v\n", audioData)
 		config := &speechpb.RecognitionConfig{
 			Encoding:        speechpb.RecognitionConfig_LINEAR16,
-			SampleRateHertz: 16000,
+			SampleRateHertz: 44100,
 			LanguageCode:    "en-US",
-		}
-	
-		// Create a new RecognitionAudio from the audio data
-		audio := &speechpb.RecognitionAudio{
-			AudioSource: &speechpb.RecognitionAudio_Content{Content: audioData},
-		}
-	
-		// Call the SpeechClient's Recognize method to transcribe the audio
+			}
+		
+			// Create a new RecognitionAudio from the audio data
+			audio := &speechpb.RecognitionAudio{
+				AudioSource: &speechpb.RecognitionAudio_Content{Content: audioData},
+			}
+		
+			// Call the SpeechClient's Recognize method to transcribe the audio
 		resp, err := speechClient.Recognize(ctx, &speechpb.RecognizeRequest{
 			Config: config,
 			Audio:  audio,
 		})
+		fmt.Printf("Response: %v\n", resp)
+		// fmt.Printf("Error: %v\n", err)
 		if err != nil {
 			fmt.Printf("Error transcribing audio: %v\n", err)
-			return
+			return ""
 		}
 	
 		// Print out the transcribed text
@@ -66,4 +71,5 @@ func main() {
 				fmt.Printf("Transcript: %v\n", alt.Transcript)
 			}
 		}
+		return resp.Results[0].Alternatives[0].Transcript
 }

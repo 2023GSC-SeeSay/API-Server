4ca3c663875a4b09ae757146a3fa8921200d7f3f
ulysseyson <ulysseyson57@gmail.com>
2023-03-08 11:39:28 +0900

    fix Route way

--- main.go
+++ main.go
@@ -11,12 +11,10 @@ func main() {
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
+		return c.SendString("<h1>SeeSay API Server</h1>")
 	})
-
-	app.Get("/:uid/audio", func(c *fiber.Ctx) error {
-		uid := c.Params("uid")
-		return c.SendString("Hello, " + uid + "!" +stt("C:\\workspace\\API-Server\\음성 002.wav", "C:\\workspace\\API-Server\\auth\\seesay-firebase-adminsdk-clpnw-faf918ab9f.json"))
+	app.Route("/api", func(r fiber.Router) {
+		r.Use("/audio", stt)
 	})
 
 	app.Listen(":3000")

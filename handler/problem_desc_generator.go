98f8ce28e548c8af6cc475d437bf9a7c774485ce
ulysseyson <ulysseyson57@gmail.com>
2023-03-28 13:05:19 +0900

    fix: 	/* This functions are not allowed for exclamation mark except "*" */

--- handler/problem_desc_generator.go
+++ handler/problem_desc_generator.go
@@ -3,19 +3,21 @@ package handler
 import (
 	"fmt"
 )
-
 func GenerateDescMouth(sentence string) string {
+	/* This functions are not allowed for exclamation mark except "*" */
 	desc := ""
-	for i, v := range sentence {
+	count := 0
+	for _, v := range sentence {
 		// fmt.Printf("%d : %c", i, v)
-		if (i/3)%3 == 1 {
+		if count%3 == 1 {
+			desc += string(v)
+			desc += "발음 :"
 			if chungsung_style[v] < 10 {
-
 				desc += chungsung_description[chungsung_style[v]]
 			} else {
 				if chungsung_style[v] < 100 {
 					desc += chungsung_description[chungsung_style[v]%10]
-					desc += "이어서 다음과 같이 발음을 합니다."
+					desc += " 이어서 다음과 같이 발음을 합니다."
 					desc += chungsung_description[chungsung_style[v]/10]
 				} else {
 					c := chungsung_style[v] / 10
@@ -26,22 +28,35 @@ func GenerateDescMouth(sentence string) string {
 			}
 			desc += "\n"
 		}
+		count++
 	}
 	fmt.Printf("%s", desc)
 	return desc
 }
 
 func GenerateDescTongue(sentence string) string {
+	fmt.Print(sentence)
+	/* This functions are not allowed for exclamation mark except "*" */
 	desc := ""
-	for i, v := range sentence {
-		if (i/3)%3 == 0 {
+	count := 0
+	for _, v := range sentence {
+		if count%3 == 0 {
+			desc += string(v)
+			desc += "발음 :"
 			desc += chosung_description[chosung_style[v]]
 			desc += "\n"
 		}
-		if (i/3)%3 == 2 {
+		if count%3 == 2 {
+			if v == '*' {
+				count ++
+				continue
+			}
+			desc += string(v)
+			desc += "발음 :"
 			desc += chongsung_description[chongsung_style[v]]
 			desc += "\n"
 		}
+		count ++
 	}
 	fmt.Printf("%s", desc)
 	return desc
--- handler/text_to_pronounce.go
+++ handler/text_to_pronounce.go
@@ -9,24 +9,31 @@ import (
 )
 
 
-func TextToPronounce() {
+func TextToPronounce(text string) string {
 	// Use python script
-	cmd, err := exec.Command("python", "C:\\workspace\\API-Server\\API-Server\\handler\\t2p.py", "--text", "안녕하세요").Output()
+	cmd, err := exec.Command("python", "C:\\workspace\\API-Server\\API-Server\\handler\\t2p.py", "--text", text).Output()
 	if err != nil {
 		panic(err)
 	}
 	s := string(cmd[2:len(string(cmd))-1])
 	s = strings.ReplaceAll(s, "\\", "")
 	s = strings.ReplaceAll(s, "x", "")
-	s = strings.ReplaceAll(s, "*", "ec83b7")
-	fmt.Printf("%v", s)
+	// ㅅ 받침은 생성되지 않으므로 ㅅ 받침 단어들로 특수문자들의 예외 처리를 해준다.
+	s = strings.ReplaceAll(s, "*", "ec83b7") //샷
+	s = strings.ReplaceAll(s, ".", "eba79b") //맛
+	s = strings.ReplaceAll(s, " ", "eb96b3") //떳
+	// fmt.Printf("%v", s)
 	b, _ := hex.DecodeString(s)
-	fmt.Printf("%v", b)
+	// fmt.Printf("%v", b)
 	ans := ""
 	for i := 0; i < len(b);  {
 		r, size := utf8.DecodeRune(b[i:i+3])
 		if string(r) == "샷" {
 			ans += "*"
+		} else if string(r) == "맛"{
+			ans += ""
+		} else if string(r) == "떳"{
+			ans += ""
 		} else {
 			ans += string(r)
 		}
@@ -34,4 +41,5 @@ func TextToPronounce() {
 
 	}
 	fmt.Printf("%v", ans)
+	return ans
 }
